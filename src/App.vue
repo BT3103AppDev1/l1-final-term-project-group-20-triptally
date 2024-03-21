a94926dcde927f40a888f0436940e12b4463bf34
Calista <calistatan03@gmail.com>
2024-03-21 23:04:30 +0800

    minor edits made

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <NavBar v-if="route.name === 'LoginPage'" />
+    <NavBar v-if="route.name === 'LoginPage' || route.name === 'SignupPage'" />
     <TopNavBar v-else />
 
   </div>
