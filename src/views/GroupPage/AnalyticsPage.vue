a49385641b5cccad382f3ece9a439601ae1de2b4
Calista <calistatan03@gmail.com>
2024-04-19 23:48:29 +0800

    created budgetanalytics and expensesanalytics components

--- src/views/GroupPage/AnalyticsPage.vue
+++ src/views/GroupPage/AnalyticsPage.vue
@@ -4,9 +4,13 @@
   <div v-if="user">
     <SideNavBar :tripName="$route.query.tripName" :tripID="$route.params.tripID"></SideNavBar>
     <div class="expenses-analytics">
-
-
+      <ExpensesAnalytics :tripID="trip.UID"/>
     </div>
+
+    <div class="budget-analytics">
+      <BudgetAnalytics/>
+    </div>
+
   </div>
   <!-- <div class="content-container">
       <h1>Analytics & Reports</h1>
@@ -35,6 +39,8 @@ import SideNavBar from './SideNavBar.vue';
 import { auth, db } from '@/firebase';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ExpensesAnalytics from './ExpensesAnalytics.vue';
+import BudgetAnalytics from './BudgetAnalytics.vue';
 
 export default {
   name: 'AnalyticsPage',
@@ -51,93 +57,7 @@ export default {
     }
   },
   methods: { 
-    async fetchExpensesData() {
-      // Fetch all the expenses logged for this trip
-      const tripRef = doc(db, "Trips", this.trip.UID);
-      const expensesRef = collection(tripRef, "Expenses");
 
-      // Create a query against the collection, ordering by the 'date' field in descending order
-      const q = query(expensesRef, orderBy("date", "desc"));
-      const querySnapshot = await getDocs(q);
-      const userCache = {};
-
-      // Helper function to fetch user details
-      const fetchUserDetails = async (userId) => {
-        if (!userCache[userId]) {
-          const userRef = doc(db, "Users", userId);
-          const userSnap = await getDoc(userRef);
-          userCache[userId] = userSnap.exists() ? userSnap.data() : { firstName: "Unknown", lastName: "" };
-        }
-        return userCache[userId];
-      };
-
-      // Fetch user details for each expense and map them
-      const expenses = await Promise.all(querySnapshot.docs.map(async (doc) => {
-        const expenseData = doc.data();
-        var userDetails; 
-        if (expenseData.paidBy === this.user.uid) { 
-          userDetails = { 
-            FirstName: "You", 
-            LastName: ""
-          }
-        } else { 
-          userDetails = await fetchUserDetails(expenseData.paidBy);
-        }
-        return {
-          id: doc.id,
-          ...expenseData,
-          date: expenseData.date.toDate(),  // Convert Timestamp to JavaScript Date
-          paidByFirstName: userDetails.FirstName, 
-          paidByLastName: userDetails.LastName
-        };
-      }));
-
-      // this function will group the expenses according to their dates 
-      this.groupExpensesByDate(expenses);
-    }, 
-    async groupExpensesByDate(expenses) { 
-      const groupedExpenses = expenses.reduce((acc, expense) => {
-      // Format the date as 'dd/mm/yyyy'
-      const expenseDate = expense.date.toLocaleDateString('en-GB');  // Change locale as needed
-
-      // Initialize the array if this date hasn't been added to the accumulator
-      if (!acc[expenseDate]) {
-        acc[expenseDate] = [];
-      }
-
-      const amountEachUserOwes = (expense.amount / (expense.owedMembers.length + 1)).toFixed(2); 
-      //console.log(expense.owedMembers);
-      var displayText; 
-
-      if (expense.owedMembers.indexOf(this.user.uid) > -1 ) { 
-        // this means that the user owes the money, so you must display ' you borrowed ....'
-        displayText = `You borrowed ${this.trip.Currency} ${amountEachUserOwes}`;
-
-      } else if (expense.paidBy === this.user.uid) { 
-        // this means that the user is the one that paid for the expense, so must display ' you lent ...' 
-        displayText = `You lent ${this.trip.Currency} ${(expense.amount - amountEachUserOwes).toFixed(2)}`;
-      } else { 
-        // this means that the user is neither the borrower nor the payer - user is not involved in the expense 
-        displayText = 'No balance';
-      }
-
-      // Push the current expense to the array for this date
-      acc[expenseDate].push({
-        id: expense.id,
-        title: expense.title,
-        category: expense.category,
-        subtitle: `${expense.paidByFirstName} ${expense.paidByLastName} paid ${expense.currency} ${(expense.amount).toFixed(2)}`,
-        sideDisplayText: displayText
-        // amount: expense.amount  // You might want to format or calculate the amount differently
-      });
-
-      return acc;
-      }, {});
-
-      // Update your component state or data object
-      this.groupedExpenses = groupedExpenses;
-      console.log(groupedExpenses);
-    }
   }, 
   setup() {
     const currency = ref("");
