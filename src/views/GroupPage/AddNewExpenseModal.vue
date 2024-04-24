cc0506f3f9e3d03898c5503d0b9f1df8c012581e
Calista <calistatan03@gmail.com>
2024-04-24 20:00:54 +0800

    debugging

--- src/views/GroupPage/AddNewExpenseModal.vue
+++ src/views/GroupPage/AddNewExpenseModal.vue
@@ -340,7 +340,7 @@ export default {
           xhr.addEventListener("readystatechange", function () {
               if (this.readyState === 4) {
                   console.log(this.responseText);
-                  this.expense.amount = this.responseText.document.inference.prediction.totalAmount.value; 
+                  this.expense.amount = this.responseText.document.inference.prediction.total_amount.value; 
                   console.log(this.responseText.document.inference.prediction.date.value)
               }
           });
